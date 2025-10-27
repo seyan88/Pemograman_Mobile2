c58481fb99fe201c4f7bfab2b34a42c47d2febab
seeyan <148548066+seyan88@users.noreply.github.com>
2025-10-28 00:11:46 +0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(MaterialApp(
     theme: ThemeData(
       useMaterial3: false, 
+      fontFamily: 'Nohemi',
       primarySwatch : Colors.deepPurple,
       scaffoldBackgroundColor: const Color.fromARGB(255, 148, 118, 230)
       ),
@@ -13,8 +14,55 @@ void main() {
         title: Text('Hello World'),
       ),
       body: Center(
-        child: Text('Hello World'),
+        child: 
+         Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Note: This app is still in development.',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Please report any issues to the developer.',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Your Feedback',
+                  hintText: 'Type your feedback here...',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              // Tombol kirim (static, belum ada logika)
+              ElevatedButton(
+                onPressed: () {
+                  // Nanti bisa tambahkan fungsi kirim di sini
+                },
+                child: Text('Submit'),
+                ),
+            ],
+            
+          ),
+        ),
+        
       ),
+      
     )
   ));
 }
